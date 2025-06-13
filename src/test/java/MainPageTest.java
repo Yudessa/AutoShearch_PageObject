2f689dfd92892e8fa3ed0ca86bea6645612f5244
Yudessa <Darksoul452@mail.ru>
2025-06-13 21:28:35 +0300

    Initial commit

---
+++ src/test/java/MainPageTest.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainPageTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        FirefoxOptions options = new FirefoxOptions();
+
+        driver = new FirefoxDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.bing.com/");
+    }
+
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+
+    @Test
+    public void search() {
+        String input = "Selenium";
+        WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
+        searchField.sendKeys(input);
+        searchField.submit();
+
+        WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
+        assertEquals(input, searchPageField.getAttribute("value"));
+    }
+}
