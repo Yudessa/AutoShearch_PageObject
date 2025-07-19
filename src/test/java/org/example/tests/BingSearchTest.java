82ce386349fa51a725a250f7ffca21d247ce92ac
Yudessa <Darksoul452@mail.ru>
2025-07-19 19:50:57 +0300

    Урок 5.2

---
+++ src/test/java/org/example/tests/BingSearchTest.java
@@ -0,0 +1,73 @@
+package org.example.tests;
+
+import org.example.pages.MainPage;
+import org.example.pages.ResultsPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BingSearchTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void setUp() {
+        FirefoxOptions options = new FirefoxOptions();
+
+        driver = new FirefoxDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.bing.com/");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    @DisplayName("Проверка перехода на selenium.dev")
+    public void searchResultsTest() {
+        String input = "Selenium";
+        MainPage mp = new MainPage(driver);
+        mp.sendText(input);
+
+
+        ResultsPage rp = new ResultsPage(driver);
+        rp.clickElement(0);
+
+        ArrayList tabs = new ArrayList<>(driver.getWindowHandles());
+        if (tabs.size() > 1) driver.switchTo().window(tabs.get(1).toString());
+
+        wait.until(ExpectedConditions.urlContains("selenium.dev"));
+        assertTrue(Objects.requireNonNull(driver.getCurrentUrl()).contains("selenium.dev"), "Неверный URL");
+    }
+
+    @Test
+    @DisplayName("поиск текста Selenium")
+    public void searchFieldTest() {
+        String input = "Selenium";
+
+        MainPage mp = new MainPage(driver);
+        mp.sendText(input);
+
+        ResultsPage rp = new ResultsPage(driver);
+        assertEquals(input, rp.getTextFromSearchField(), "Текст не совпал");
+    }
+}
+
